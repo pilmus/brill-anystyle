ec4bdf4a3b10e8b48abbaf921b9ed2ca12b74060
Maaike Visser <m.e.b.p.visser@student.tudelft.nl>
2017-07-10 16:12:37 +0200

    Add files

--- training_files/true.rb
+++ training_files/true.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
 require 'anystyle/parser'
-Anystyle.parser.train 'originele_training_file_plus_brill_v2.txt', true
-Anystyle.parser.model.path="./origineel_plus_brill.mod"
+Anystyle.parser.train 'train.txt', true
+Anystyle.parser.model.path="./een_file.mod"
 Anystyle.parser.model.save
