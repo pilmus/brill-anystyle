1920358a85da664e5cc0794b6be5d226b12b4f85
Maaike Visser <m.e.b.p.visser@student.tudelft.nl>
2017-07-10 14:12:46 +0200

    Update gitignore to NOT ignore certain xmls

--- scripts/true.rb
+++ scripts/true.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
 require 'anystyle/parser'
-Anystyle.parser.train 'originele_plus_brill_v2', true
-Anystyle.parser.model.path="./modelgroundup.mod"
+Anystyle.parser.train 'originele_training_file_plus_brill_v2.txt', true
+Anystyle.parser.model.path="./origineel_plus_brill.mod"
 Anystyle.parser.model.save
